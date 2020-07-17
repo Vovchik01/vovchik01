745b800c13986d92ddf70b63bb266a10b32f01a2
Владимир Елезаров <wavilla23@gmail.com>
2020-07-17 17:07:56 +0300

    Initial commit

---
+++ src/main/kotlin/VideoList.kt
@@ -0,0 +1,39 @@
+import kotlinx.html.js.onClickFunction
+import kotlinx.html.video
+import react.*
+import react.dom.*
+import kotlin.browser.window
+
+
+class VideoList : RComponent<VideoListProps, RState>() {
+    override fun RBuilder.render() {
+        for(video in props.videos) {
+            p {
+                key = video.id.toString()
+                attrs {
+                    onClickFunction = {
+                        props.onSelectVideo(video)
+                    }
+                }
+                if (video == props.selectedVideo) {
+                    +"▶ "
+                }
+                +"${video.speaker}: ${video.title}"
+            }
+        }
+    }
+}
+
+external interface VideoListProps: RProps {
+    var videos: List<Video>
+    var selectedVideo: Video?
+    var onSelectVideo: (Video) -> Unit
+}
+
+data class Video(val id: Int, val title: String, val speaker: String, val videoUrl: String)
+
+fun RBuilder.videoList(handler: VideoListProps.() -> Unit): ReactElement {
+    return child(VideoList::class) {
+        this.attrs(handler)
+    }
+}
