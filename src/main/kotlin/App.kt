332ba3dbe9ee1c77a57b0a86c7282ae658f9ab54
Владимир Елезаров <wavilla23@gmail.com>
2020-07-18 10:37:49 +0300

    step 8

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,5 +1,7 @@
+import kotlinx.coroutines.*
 import react.*
 import react.dom.*
+import kotlin.browser.window
 
 class App : RComponent<RProps, AppState>() {
 
@@ -55,15 +57,16 @@ class App : RComponent<RProps, AppState>() {
         }
     }
     override fun AppState.init() {
-        unwatchedVideos = listOf(
-                Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-                Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-                Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-        )
+        unwatchedVideos = listOf()
+        watchedVideos = listOf()
 
-        watchedVideos = listOf(
-                Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-        )
+        val mainScope = MainScope()
+        mainScope.launch {
+            val videos = fetchVideos()
+            setState{
+                unwatchedVideos = videos
+            }
+        }
     }
 }
 
@@ -71,6 +74,21 @@ external interface AppState : RState {
     var currentVideo : Video?
     var unwatchedVideos: List<Video>
     var watchedVideos: List<Video>
+}
+
+suspend fun fetchVideo(id: Int): Video =
+    window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+            .await()
+            .json()
+            .await()
+            .unsafeCast<Video>()
+
+suspend fun fetchVideos(): List<Video> = coroutineScope {
+    (1..25).map {id ->
+        async {
+            fetchVideo(id)
+        }
+    }.awaitAll()
 }
 
 
